c20b45c511320c1e608555e0b21d6294e25e84c1
AtimaB <nickzehn10@hotmail.com>
2020-07-30 17:08:09 -0600

    test team.html file

--- Develop/app.js
+++ Develop/app.js
@@ -5,11 +5,11 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
+const OUTPUT_DIR = path.resolve(__dirname, "team.html");
+// const OUTPUT_DIR = path.resolve(__dirname, "output");
+// const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
-// const Employee = require("./lib/Employee");
 
 const teamMember = [];
 
