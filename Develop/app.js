a4536d18bdf90ddf11493463fff79d5602b4e060
AtimaB <nickzehn10@hotmail.com>
2020-07-31 12:28:22 -0600

    generate html file inside output folder

--- Develop/app.js
+++ Develop/app.js
@@ -5,9 +5,9 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
-const OUTPUT_DIR = path.resolve(__dirname, "team.html");
-// const OUTPUT_DIR = path.resolve(__dirname, "output");
-// const outputPath = path.join(OUTPUT_DIR, "team.html");
+// const OUTPUT_DIR = path.resolve(__dirname, "team.html");
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
@@ -251,9 +251,9 @@ function createIntern() {
 }
 
 function createHtml (){
-    fs.writeFileSync(OUTPUT_DIR, render(teamMember), "utf-8");
-    // fs.writeFileSync(outputPath, render(teamMember), "utf-8")
-    console.log ("Successfully created your team!")
+    // fs.writeFileSync(OUTPUT_DIR, render(teamMember), "utf-8");
+    fs.writeFileSync(outputPath, render(teamMember), "utf-8")
+    console.log ("Successfully created team.html! (located inside output folder.)")
 } 
 
 
